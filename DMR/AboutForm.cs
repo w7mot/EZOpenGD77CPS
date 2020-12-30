6e7462d9f45cb2b82ae9df8b1402f4678c1897be
Roger Clark <roger@rogerclark.net>
2020-12-30 20:43:58 +1100

    Remove 'Company' text from the About screen

--- DMR/AboutForm.cs
+++ DMR/AboutForm.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -7,19 +6,12 @@ namespace DMR
 {
     public class AboutForm : Form
     {
-        //private IContainer components;
-
         private Label lblVersion;
-
-        private Label lblCompany;
         private Label lblTranslationCredit;
-
         private Button btnClose;
 
         public AboutForm()
         {
-
-            //base._002Ector();
             this.InitializeComponent();
             this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);// Roger Clark. Added correct icon on main form!
             base.Scale(Settings.smethod_6());
@@ -29,7 +21,6 @@ namespace DMR
         {
             Settings.smethod_68(this);
             this.lblVersion.Text = "OpenGD77 CPS";
-            this.lblCompany.Text += "\n\nRoger VK3KYY/G4KYF\nColin G4EML\nJason VK7ZJA\nMike DL2MF";
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -39,17 +30,12 @@ namespace DMR
 
         protected override void Dispose(bool disposing)
         {
-            /*	if (disposing && this.components != null)
-                {
-                    this.components.Dispose();
-                }*/
             base.Dispose(disposing);
         }
 
         private void InitializeComponent()
         {
             this.lblVersion = new System.Windows.Forms.Label();
-            this.lblCompany = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
             this.lblTranslationCredit = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -63,19 +49,9 @@ namespace DMR
             this.lblVersion.Text = "v1.0.0";
             this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // lblCompany
-            // 
-            this.lblCompany.AutoSize = true;
-            this.lblCompany.Location = new System.Drawing.Point(100, 61);
-            this.lblCompany.Name = "lblCompany";
-            this.lblCompany.Size = new System.Drawing.Size(68, 16);
-            this.lblCompany.TabIndex = 0;
-            this.lblCompany.Text = "Company";
-            this.lblCompany.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(173, 248);
+            this.btnClose.Location = new System.Drawing.Point(175, 60);
             this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(64, 27);
@@ -94,9 +70,8 @@ namespace DMR
             // 
             // AboutForm
             // 
-            this.ClientSize = new System.Drawing.Size(409, 299);
+            this.ClientSize = new System.Drawing.Size(409, 104);
             this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.lblCompany);
             this.Controls.Add(this.lblTranslationCredit);
             this.Controls.Add(this.lblVersion);
             this.Font = new System.Drawing.Font("Arial", 10F);
@@ -107,8 +82,6 @@ namespace DMR
             this.Text = "About";
             this.Load += new System.EventHandler(this.AboutForm_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
     }
 }
